6e3d553ac820e8b75e414d439bede2714a1870bc
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 18:16:40 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -83,14 +83,14 @@ st.markdown("Manuel Hanono y Bruno Soifer.")
 st.sidebar.header("Filtrar por:")
 
 # Create a list of possible values and multiselect menu with them in it.
-YEARS = data['YEAR'].unique()
+YEARS = data['YEAR'].unique().sort_values()
 container = st.container()
 all = st.sidebar.checkbox("Todos los años")
 if all:
-    YEARS_SELECTED = container.multiselect("Seleccionar uno o mas años:",
+    YEARS_SELECTED = sidebar.container.multiselect("Seleccionar uno o mas años:",
          YEARS,YEARS)
 else:
-    YEARS_SELECTED =  container.multiselect("Seleccionar uno o mas años:",
+    YEARS_SELECTED =  sidebar.container.multiselect("Seleccionar uno o mas años:",
         YEARS)
 # Mask to filter dataframe
 mask_years = data['YEAR'].isin(YEARS_SELECTED)
