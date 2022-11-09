cdc91157fb75d1ec4505fa544c753cf64c4715b8
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 18:20:36 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -88,11 +88,9 @@ container = st.container()
 with st.sidebar:
     all = st.checkbox("Todos los años")
     if all:
-        YEARS_SELECTED = container.multiselect("Seleccionar uno o mas años:",
-         YEARS,YEARS)
+        YEARS_SELECTED = container.multiselect("Seleccionar uno o mas años:", YEARS,YEARS)
     else:
-        YEARS_SELECTED =  container.multiselect("Seleccionar uno o mas años:",
-        YEARS)
+        YEARS_SELECTED =  container.multiselect("Seleccionar uno o mas años:", YEARS)
 # Mask to filter dataframe
 mask_years = data['YEAR'].isin(YEARS_SELECTED)
 data = data[mask_years]
