928157577433b51268c2fdd692e580fc56fa7d45
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 11:26:16 -0300

    Update Proyecto I.py

--- Proyecto I.py
+++ Proyecto I.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-
 
 st.title("Proyecto I - Manuel Hanono y Bruno Soifer")
-
-window = st.slider("Holaaa")
-
-
-
-
-print("Corrio")
+st.ballons()
