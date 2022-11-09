adf5492c54396dba80cf1ae4f4e8267183465d3a
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 11:36:03 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('/Users/manu/Downloads/Motor_Vehicle_Collisions_-_Crashes.csv', low_memory = False)
 
 st.title("Proyecto I - Manuel Hanono y Bruno Soifer")
-st.ballons()
