4ba578272c1e2973c9645613893062c3cc4bfc0b
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-10 00:03:15 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -76,15 +76,15 @@ data['YEAR'] = pd.DatetimeIndex(data['DATE']).year
 data['MONTH'] = pd.DatetimeIndex(data['DATE']).month
 data['CANT_AUTOS'] = 0
 
-if data["VEHICLE TYPE CODE 1"].to_string().empty:
+if data["VEHICLE TYPE CODE 1"].to_string() == "":
     data['CANT_AUTOS'] = 0
-elif data["VEHICLE TYPE CODE 2"].isnull():
+elif data["VEHICLE TYPE CODE 2"].to_string() == "":
     data['CANT_AUTOS'] = 1
-elif data["VEHICLE TYPE CODE 3"].isnull():
+elif data["VEHICLE TYPE CODE 3"].to_string() == "":
     data['CANT_AUTOS'] = 2
-elif data["VEHICLE TYPE CODE 4"].isnull():
+elif data["VEHICLE TYPE CODE 4"].to_string() == "":
     data['CANT_AUTOS'] = 3
-elif data["VEHICLE TYPE CODE 5"].isnull():
+elif data["VEHICLE TYPE CODE 5"].to_string() == "":
     data['CANT_AUTOS'] = 4
 else:
     data['CANT_AUTOS'] = 5
