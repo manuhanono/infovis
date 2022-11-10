ff39ee5619ff3ad8933d93dc906be881246cbd83
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-10 16:48:41 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -65,6 +65,7 @@ def load_data(rows):
     df25 = pd.read_csv(DATA_URL25)
     data=pd.concat([df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13,df14,df15,df16,df17,df18,df19,df20,df21,df22,df23,df24,df25],ignore_index=True)
     data = data.head(rows)
+    return data
 
 data = load_data(100000)
 
