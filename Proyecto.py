aa7c70d301af7160944cf0498a74c92d5d15d68c
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 22:34:41 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -110,35 +110,6 @@ st.header("Where are the most people injured in NYC?")
 #injured_people = st.slider("Number of persons injured in NYC",0,19)
 st.map(data.query("lat <= 41 & lat > 39")[['lat', 'lon']].dropna(how="any"))
 
-
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
     
 st.header("AA")
 st.bar_chart(data=data, x="PINJ", y="BOROUGH")
