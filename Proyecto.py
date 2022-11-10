28e42c89bed69f352472cf2453ecf6d157dc92a6
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-09 23:35:48 -0300

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -113,27 +113,6 @@ st.header("Where are the most people injured in NYC?")
 st.map(data.query("lat <= 41 & lat > 39")[['lat', 'lon']].dropna(how="any"))
 
 
-df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/minoritymajority.csv')
-df_sample_r = df_sample[df_sample['STNAME'] == 'New York']
-
-values = df_sample_r['TOT_POP'].tolist()
-fips = df_sample_r['FIPS'].tolist()
-
-endpts = list(np.mgrid[min(values):max(values):4j])
-colorscale = ["#030512","#1d1d3b","#323268","#3d4b94","#3e6ab0",
-              "#4989bc","#60a7c7","#85c5d3","#b7e0e4","#eafcfd"]
-fig = ff.create_choropleth(
-    fips=fips, values=values, scope=['New York'], show_state_data=True,
-    colorscale=colorscale, binning_endpoints=endpts, round_legend_values=True,
-    plot_bgcolor='rgb(229,229,229)',
-    paper_bgcolor='rgb(229,229,229)',
-    legend_title='Population by County',
-    county_outline={'color': 'rgb(255,255,255)', 'width': 0.5},
-    exponent_format=True,
-)
-fig.layout.template = None
-fig.show()
-
 st.header("AA")
 st.bar_chart(data=data, x="PINJ", y="BOROUGH")
 # make a dropdown search
